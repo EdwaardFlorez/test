9ec8a4f587f180570b39e29b1593dcf95cd9d077
Edward Flórez <edwardflorez1@hotmail.com>
2022-07-29 18:05:09 -0500

    header and home component added

---
+++ src/components/organisms/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <h2>test home</h2>
+    </div>
+  );
+};
+
+export default Home;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
+/* react router dom v6 */
+import { Route, Routes as RoutesDom } from "react-router-dom";
+
+/* components */
+import Header from "../components/organisms/Header";
+import Home from "../components/organisms/Home";
+
 const Routes = () => {
-    return (
-        <h1>firs test</h1>
-    )
-}
+  return (
+    <div className="Routes w-screen h-screen flex flex-col">
+      <div className="header-container h-20 ">
+        <Header></Header>
+      </div>
+      <div className="main-container grow ">
+        <RoutesDom>
+          <Route path="/" element={<Home />} />
+        </RoutesDom>
+      </div>
+    </div>
+  );
+};
 
 export default Routes;
