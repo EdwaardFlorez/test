08e13939e4c81316291a1c5509c90320fae44ead
Edward Flórez <edwardflorez1@hotmail.com>
2022-07-29 19:48:00 -0500

    first mount carousel

--- src/components/organisms/Home.jsx
+++ src/components/organisms/Home.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 /* components */
 import PaginatorPrime from "../molecules/paginator/PaginatorPrime";
-
+import Carousel from "../molecules/carousel/Carousel";
 
 const Home = () => {
   return (
     <div className="Home">
+      <div className="carousel-container">
+        <Carousel></Carousel>
+      </div>
       <div className="paginator-container">
         <PaginatorPrime></PaginatorPrime>
       </div>
